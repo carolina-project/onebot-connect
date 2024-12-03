a47f1fa110cfd89d81145570a48f81d788e2a444
arkuna23 <nightalpha23@gmail.com>
2024-12-03 11:39:32 +0800

    🚧 Interface

--- onebot-connect-interface/src/client.rs
+++ onebot-connect-interface/src/client.rs
@@ -8,20 +8,39 @@ use onebot_types::{
 
 #[cfg(feature = "client_recv")]
 mod recv {
-    use futures::channel::mpsc;
+    use futures::channel::{mpsc, oneshot};
     use serde::{Deserialize, Serialize};
 
     use super::*;
 
     /// Messages received from OneBot Connect
-    #[derive(Debug, Serialize, Deserialize)]
+    #[derive(Debug)]
     pub enum RecvMessage {
         Event(Event),
         /// Response after closed
         Closed(Result<(), String>),
     }
+
+    /// Action args passed to OneBot Connect with command channel
+    /// Response will be sent using `resp_tx`
+    pub struct ActionArgs {
+        pub action: ActionType,
+        pub self_: Option<BotSelf>,
+        pub resp_tx: oneshot::Sender<Result<Value, Error>>,
+    }
+
+    /// Command enum to represent different commands that can be sent to OneBot Connect
+    pub enum Command {
+        Action(ActionArgs),
+        Close,
+    }
     pub type MessageRecv = mpsc::Receiver<RecvMessage>;
 
+    pub trait Event {
+        
+    }
+
+    /// Trait to define the connection behavior for OneBot Connect
     pub trait Connect {
         type CErr: ErrTrait;
         type Client: Client;
@@ -34,17 +53,72 @@ mod recv {
 pub use recv::*;
 use serde_value::Value;
 
+/// OneBot application client, providing functions to interact with OneBot Connect
 pub trait Client {
     fn send_action_impl(
         &mut self,
         action: ActionType,
         self_: Option<BotSelf>,
-    ) -> Pin<Box<dyn Future<Output = Result<Value, Error>> + Send>>;
+    ) -> impl Future<Output = Result<Value, Error>> + Send + 'static;
 
-    fn close(&mut self) -> Pin<Box<dyn Future<Output = Result<(), String>> + Send>>;
+    fn close_impl(&mut self) -> impl Future<Output = Result<(), String>> + Send + 'static;
 }
 
-pub trait ClientExt: Client {
+pub trait ClientDyn {
+    fn send_action_dyn(
+        &mut self,
+        action: ActionType,
+        self_: Option<BotSelf>,
+    ) -> Pin<Box<dyn Future<Output = Result<Value, Error>> + Send>>;
+
+    fn close_dyn(&mut self) -> Pin<Box<dyn Future<Output = Result<(), String>> + Send>>;
+}
+
+impl<T: Client> ClientDyn for T {
+    fn send_action_dyn(
+        &mut self,
+        action: ActionType,
+        self_: Option<BotSelf>,
+    ) -> Pin<Box<dyn Future<Output = Result<Value, Error>> + Send>> {
+        Box::pin(self.send_action_impl(action, self_))
+    }
+
+    fn close_dyn(&mut self) -> Pin<Box<dyn Future<Output = Result<(), String>> + Send>> {
+        Box::pin(self.close_impl())
+    }
+}
+
+pub trait ClientExt {
+    fn send_action<E, A>(
+        &mut self,
+        action: A,
+        self_: Option<BotSelf>,
+    ) -> impl Future<Output = Result<A::Resp, Error>>
+    where
+        E: std::error::Error + 'static,
+        A: OBAction + TryInto<ActionType, Error = E>;
+}
+
+impl<T: Client> ClientExt for T {
+    fn send_action<E, A>(
+        &mut self,
+        action: A,
+        self_: Option<BotSelf>,
+    ) -> impl Future<Output = Result<A::Resp, Error>>
+    where
+        E: std::error::Error + 'static,
+        A: OBAction + TryInto<ActionType, Error = E>,
+    {
+        async move {
+            let resp = self
+                .send_action_impl(action.try_into().map_err(Error::other)?, self_)
+                .await?;
+            Ok(<A::Resp as serde::Deserialize>::deserialize(resp)?)
+        }
+    }
+}
+
+impl ClientExt for dyn ClientDyn {
     fn send_action<E, T>(
         &mut self,
         action: T,
@@ -56,12 +130,9 @@ pub trait ClientExt: Client {
     {
         async move {
             let resp = self
-                .send_action_impl(action.try_into().map_err(Error::other)?, self_)
+                .send_action_dyn(action.try_into().map_err(Error::other)?, self_)
                 .await?;
             Ok(<T::Resp as serde::Deserialize>::deserialize(resp)?)
         }
     }
 }
-
-impl<T: Client> ClientExt for T {}
-impl ClientExt for dyn Client {}
